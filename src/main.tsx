7518522f7f575f7f99edd08abbb24796a534290e
Himanshuweezy <sudanhghg@gmail.com>
2021-07-16 15:21:03 +0545

    V.1.1 React import before used bug fixed

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-use-before-define */
+
+import React from 'react'
+import { render } from 'react-dom'
+
+import {App} from './App'
+
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
